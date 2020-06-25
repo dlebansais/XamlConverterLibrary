21a7c2321509f820e77618d3ba224f0628fdd585
dlebansais <dlebansais@gmail.com>
2020-06-25 22:23:07 +0200

    Add project files.

---
+++ XamlConverterLibrary/NullToObjectConverter.cs
@@ -0,0 +1,68 @@
+﻿namespace Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Converter from a reference to the first or second object of a collection.
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(object))]
+    [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instanciated in Xaml")]
+    public class NullToObjectConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converter from a reference to the first or second object of a collection.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// If <paramref name="value"/> is non-null reference, and <paramref name="parameter"/> is a collection with at least two objects, the converter returns the second object in the collection.
+        /// Otherwise, if collection has at least two objects, the converter returns the first object in the collection.
+        /// Otherwise, this method throws an exception.
+        /// </returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, parameter);
+        }
+
+        /// <summary>
+        /// Converter from a reference to the first or second object of a collection.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <returns>
+        /// If <paramref name="value"/> is non-null reference, and <paramref name="parameter"/> is a collection with at least two objects, the converter returns the second object in the collection.
+        /// Otherwise, if collection has at least two objects, the converter returns the first object in the collection.
+        /// Otherwise, this method throws an exception.
+        /// </returns>
+        public static object Convert(object value, object parameter)
+        {
+            bool NullValue = value == null;
+
+            if (parameter is CompositeCollection CollectionOfItems && CollectionOfItems.Count > 1)
+                return NullValue ? CollectionOfItems[0] : CollectionOfItems[1];
+            else
+                throw new ArgumentOutOfRangeException(nameof(parameter));
+        }
+
+        /// <summary>
+        /// Converts an object to an <see cref="object"/> reference in a binding.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value.</returns>
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is CompositeCollection CollectionOfItems && CollectionOfItems.Count > 1)
+                return value == CollectionOfItems[1] ? BindingOperations.DisconnectedSource : null;
+            else
+                throw new ArgumentOutOfRangeException(nameof(parameter));
+        }
+    }
+}
---
+++ XamlConverterLibrary/ZeroToObjectConverter.cs
@@ -0,0 +1,110 @@
+﻿namespace Converters
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Converter from an int (or convertible) or nullable int to the first or second object of a collection.
+    /// </summary>
+    [ValueConversion(typeof(int), typeof(object))]
+    [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instanciated in Xaml")]
+    public class ZeroToObjectConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converter from an int (or convertible) or nullable int to the first or second object of a collection.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// If <paramref name="parameter"/> is a collection with at least three objects, and <paramref name="value"/> is null, returns the 3rd item.
+        /// Otherwise, If <paramref name="value"/> is an int (or convertible) and non-zero, or an int? and also non-zero, and <paramref name="parameter"/> is a collection with at least two objects, the converter returns the second object in the collection.
+        /// Otherwise, if the collection has at least two objects, the converter returns the first object in the collection.
+        /// Otherwise, this method throws an exception.
+        /// </returns>
+        public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, parameter);
+        }
+
+        /// <summary>
+        /// Converter from an int (or convertible) or nullable int to the first or second object of a collection.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <returns>
+        /// If <paramref name="parameter"/> is a collection with at least three objects, and <paramref name="value"/> is null, returns the 3rd item.
+        /// Otherwise, If <paramref name="value"/> is an int (or convertible) and non-zero, or an int? and also non-zero, and <paramref name="parameter"/> is a collection with at least two objects, the converter returns the second object in the collection.
+        /// Otherwise, if the collection has at least two objects, the converter returns the first object in the collection.
+        /// Otherwise, this method throws an exception.
+        /// </returns>
+        public static object Convert(object? value, object parameter)
+        {
+            if (value == null && parameter is CompositeCollection CollectionOfThreeItems && CollectionOfThreeItems.Count > 2)
+                return CollectionOfThreeItems[2];
+
+            int IntValue;
+
+            if (value is int)
+                IntValue = (int)value;
+            else if (value is byte)
+                IntValue = (byte)value;
+            else if (value is sbyte)
+                IntValue = (sbyte)value;
+            else if (value is short)
+                IntValue = (short)value;
+            else if (value is ushort)
+                IntValue = (ushort)value;
+            else if (value is uint)
+                IntValue = (int)(uint)value;
+            else if (value is long)
+                IntValue = (int)(long)value;
+            else if (value is ulong)
+                IntValue = (int)(ulong)value;
+            else if (value is int?)
+                IntValue = ((int?)value) ?? 0;
+            else if (value is byte?)
+                IntValue = ((byte?)value) ?? 0;
+            else if (value is sbyte?)
+                IntValue = ((sbyte?)value) ?? 0;
+            else if (value is short?)
+                IntValue = ((short?)value) ?? 0;
+            else if (value is ushort?)
+                IntValue = ((ushort?)value) ?? 0;
+            else if (value is uint?)
+                IntValue = (int)(((uint?)value) ?? 0);
+            else if (value is long?)
+                IntValue = (int)(((long?)value) ?? 0);
+            else if (value is ulong?)
+                IntValue = (int)(((ulong?)value) ?? 0);
+            else
+                throw new ArgumentOutOfRangeException(nameof(value));
+
+            if (parameter is CompositeCollection CollectionOfItems && CollectionOfItems.Count > 1)
+                return IntValue != 0 ? CollectionOfItems[1] : CollectionOfItems[0];
+            else
+                throw new ArgumentOutOfRangeException(nameof(parameter));
+        }
+
+        /// <summary>
+        /// Converts an object to an <see cref="int"/> instance in a binding.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value.</returns>
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is CompositeCollection CollectionOfThreeItems && CollectionOfThreeItems.Count > 2 && value == CollectionOfThreeItems[2])
+                return null;
+            else if (parameter is CompositeCollection CollectionOfItems && CollectionOfItems.Count > 1)
+                return value == CollectionOfItems[1] ? 1 : 0;
+            else
+                throw new ArgumentOutOfRangeException(nameof(parameter));
+        }
+    }
+}
