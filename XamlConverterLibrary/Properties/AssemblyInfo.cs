b58ecefb61b6d538e84e8e01c4303fb09baa1ec8
dlebansais <dlebansais@gmail.com>
2020-10-04 23:13:05 +0200

    Updated to .NET Core project format.

--- XamlConverterLibrary/Properties/AssemblyInfo.cs
+++ XamlConverterLibrary/Properties/AssemblyInfo.cs
@@ -1,23 +1,3 @@
-﻿using System;
-using System.Reflection;
-using System.Resources;
-using System.Runtime.InteropServices;
-using System.Windows.Markup;
+﻿using System.Windows.Markup;
 
 [assembly: XmlnsDefinition("Converters", "XamlConverterLibrary")]
-
-[assembly: AssemblyTitle("XamlConverterLibrary")]
-[assembly: AssemblyDescription("Library of converters for Xaml binding")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XamlConverterLibrary")]
-[assembly: AssemblyCopyright("Copyright © 2019 David Le Bansais")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("fcc85148-4d60-468c-92d6-4690616f8cd1")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
