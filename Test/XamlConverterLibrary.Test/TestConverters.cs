4d7765b2f4b3dfbef97b7fb6151cf8d1188f21f4
David Le Bansais <dlebansais@gmail.com>
2023-12-27 00:02:03 +0100

    Update to .NET 8 and C# 12.

---
+++ Test/XamlConverterLibrary.Test/TestConverters.cs
@@ -0,0 +1,17 @@
+﻿namespace XamlConverterLibrary.Test;
+
+using System;
+using System.Threading;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestConverters
+{
+    [Test]
+    [Apartment(ApartmentState.STA)]
+    public void TestSuccess()
+    {
+        TestWindow Dlg = new();
+        Dlg.Show();
+    }
+}
---
+++ Test/XamlConverterLibrary.Test/TestWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace XamlConverterLibrary.Test;
+
+using System.Windows;
+
+/// <summary>
+/// Interaction logic for TestWindow.xaml
+/// </summary>
+public partial class TestWindow : Window
+{
+    public TestWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+}
