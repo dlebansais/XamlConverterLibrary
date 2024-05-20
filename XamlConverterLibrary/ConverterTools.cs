2eb2614274bcdda9906580ded86447f278a7aa9c
David Le Bansais <dlebansais@gmail.com>
2024-05-20 13:14:42 +0200

    CI fix.

--- XamlConverterLibrary/ConverterTools.cs
+++ XamlConverterLibrary/ConverterTools.cs
@@ -43,7 +43,7 @@ internal static partial class ConverterTools
     private static bool CanCreateInstanceOfVerified(this Type targetType)
     {
         var Fields = targetType.GetFields();
-        var StaticFieldInfo = Fields?.FirstOrDefault(IsStaticInstance);
+        var StaticFieldInfo = Fields.FirstOrDefault(IsStaticInstance);
 
         var Constructors = targetType.GetConstructors();
         var ParameterlessConstructorInfo = Constructors.FirstOrDefault((ConstructorInfo constructor) => constructor.GetParameters().Length == 0);
